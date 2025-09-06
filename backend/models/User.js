6e54d2822fe0ac26d0f72e52352c7a2971599190
jayashre-25 <dasjayashree8260@gmail.com>
2025-09-06 14:26:17 +0530

    feat: update user schema to conditionally require password based on googleId

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  password: { type: String, 
+    required: function(){
+        return this.googleId ? false : true;
+    } },
   role: { type: String, default: 'user' },
+  googleId: { type: String, default: null },
 });
 
 const User = mongoose.model('User', userSchema);
